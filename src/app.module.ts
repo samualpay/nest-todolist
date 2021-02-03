7f1d6393fd3a93ae41141d9c9587c0772e6fcd3f
star <star@funpodium.net>
2021-02-03 11:40:46 +0800

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodolistModule } from './todolist/todolist.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TodolistModule],
+  imports: [TodolistModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
